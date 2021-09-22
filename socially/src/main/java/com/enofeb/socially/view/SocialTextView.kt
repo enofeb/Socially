178fd159fdcffd84a6971f922ec73dd7ef6b4704
Enes Zor <enes.zor@leanscale.com>
2021-09-22 23:28:46 +0300

    build tools version was updated

--- socially/src/main/java/com/enofeb/socially/view/SocialTextView.kt
+++ socially/src/main/java/com/enofeb/socially/view/SocialTextView.kt
@@ -2,6 +2,7 @@ package com.enofeb.socially.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import androidx.appcompat.widget.AppCompatTextView
 import com.enofeb.socially.R
 
@@ -11,4 +12,12 @@ class SocialTextView @JvmOverloads constructor(
     defStyleAttr: Int = R.attr.socialTextViewStyle
 ) : AppCompatTextView(context, attrs, defStyleAttr) {
 
+    init {
+        this.isClickable = true
+
+        val words = this.text.split("\\s+".toRegex()).map { word ->
+            word.replace("""^[,\.]|[,\.]$""".toRegex(), "")
+        }
+        Log.e("TEXTCHECK", words.toString())
+    }
 }

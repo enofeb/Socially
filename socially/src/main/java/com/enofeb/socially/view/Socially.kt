5472fa60862ede3b3ba6d0d4bf313cda8698e5ce
Enes Zor <enes.zor@leanscale.com>
2021-09-22 21:39:11 +0300

    SocialTextView was created

---
+++ socially/src/main/java/com/enofeb/socially/view/Socially.kt
@@ -0,0 +1,14 @@
+package com.enofeb.socially.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.enofeb.socially.R
+
+class SocialTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.socialTextViewStyle
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+}
